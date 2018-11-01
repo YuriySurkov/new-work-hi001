aeaccecdf9ab23d049baabb4ba88c790ec747a7e
YuriySurkov <43704706+YuriySurkov@users.noreply.github.com>
2018-11-01 12:45:25 +0300

    Create 001.js

---
+++ 001.js
@@ -0,0 +1,13 @@
+var i = 0;
+ 
+for (i=0;i<17;i++) {
+    var div1 = document.createElement('div');
+    document.body.appendChild(div1);
+    div1.setAttribute("id", "block1");
+    };
+ 
+for (i=0;i<17;i++) {
+    var div2 = document.createElement('div');
+    document.body.appendChild(div2);
+    div2.setAttribute("id", "block2");
+    };
